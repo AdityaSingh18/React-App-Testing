73da801c4ab41ba25cd5a4f680d408eb96c6eced
Aditya <aditya18.mail@gmail.com>
2023-04-01 19:03:34 +0530

    using async

--- src/Component/Pages/Async.test.js
+++ src/Component/Pages/Async.test.js
@@ -3,6 +3,10 @@ import ExpenseTracker from './ExpenseTracker';
 
 describe('Async component',()=>{
 test('renders posts if request succeeds',async()=>{
+    window.fetch= jest.fn();
+    window.fetch.mockResolvedValueOnce({
+        JSON:async()=>[{id:'p1',title:'First Post'} ]
+    });
     render(<ExpenseTracker/>)
 
     const listItemElements = await screen.findAllByRole('listitem');
